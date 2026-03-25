0775113899030b363ab4633eb5ac212f5207743a
Kai Harris <alienflipsf@gmail.com>
2026-03-25 18:41:39 +0000

    Use all bits of ui_in

--- src/project.sv
+++ src/project.sv
@@ -22,7 +22,7 @@ module tt_um_tcpu_alienflip (
   assign uio_oe      = '0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, uio_in[7:0], 1'b0};
+  wire _unused = &{ena, uio_in[7:0], ui_in[7:1], 1'b0};
 
   control control_u (
     .clk(clk),
--- src/uart_rx.sv
+++ src/uart_rx.sv
@@ -39,80 +39,65 @@ end
 // RX state machine
 always @(posedge i_Clock or negedge rst_n) begin
     if (!rst_n) begin
-        o_Rx_DV        <= 1'b0;
-        o_Rx_Byte      <= 8'd0;
-
-        r_Rx_Data_R    <= 1'b1;
-        r_Rx_Data      <= 1'b1;
-
-        r_Clock_Count  <= '0;
-        r_Bit_Index    <= '0;
-        r_Rx_Byte      <= '0;
-
-        r_SM_Main      <= s_IDLE;
+        o_Rx_Byte <= '0;
+        o_Rx_DV       <= 1'b0;
+        r_Clock_Count <= 0;
+        r_Bit_Index   <= 0;
     end else begin
-        // double-flop already exists (separate always block)
-
         case (r_SM_Main)
-
             s_IDLE: begin
-                o_Rx_DV       <= 1'b0;
-                r_Clock_Count <= '0;
-                r_Bit_Index   <= '0;
+            o_Rx_DV       <= 1'b0;
+            r_Clock_Count <= 0;
+            r_Bit_Index   <= 0;
 
-                if (r_Rx_Data == 1'b0)
-                    r_SM_Main <= s_RX_START_BIT;
+            if (r_Rx_Data == 1'b0)
+                r_SM_Main <= s_RX_START_BIT;
             end
 
             s_RX_START_BIT: begin
                 if (r_Clock_Count == (CLKS_PER_BIT-1)/2) begin
                     if (r_Rx_Data == 1'b0) begin
-                        r_Clock_Count <= '0;
+                        r_Clock_Count <= 0;
                         r_SM_Main     <= s_RX_DATA_BITS;
-                    end else begin
+                    end else
                         r_SM_Main <= s_IDLE;
-                    end
                 end else begin
-                    r_Clock_Count <= r_Clock_Count + 1'b1;
+                    r_Clock_Count <= r_Clock_Count + 'd1;
                 end
             end
 
             s_RX_DATA_BITS: begin
                 if (r_Clock_Count < CLKS_PER_BIT-1) begin
-                    r_Clock_Count <= r_Clock_Count + 1'b1;
+                    r_Clock_Count <= r_Clock_Count + 'd1;
                 end else begin
-                    r_Clock_Count         <= '0;
-                    r_Rx_Byte[r_Bit_Index] <= r_Rx_Data;
-
+                        r_Clock_Count           <= 0;
+                        r_Rx_Byte[r_Bit_Index]  <= r_Rx_Data;
                     if (r_Bit_Index < 7) begin
-                        r_Bit_Index <= r_Bit_Index + 1'b1;
+                        r_Bit_Index <= r_Bit_Index + 'd1;
                     end else begin
-                        r_Bit_Index <= '0;
+                        r_Bit_Index <= 0;
                         r_SM_Main   <= s_RX_STOP_BIT;
                     end
                 end
             end
 
             s_RX_STOP_BIT: begin
-                if (r_Clock_Count < CLKS_PER_BIT-1) begin
-                    r_Clock_Count <= r_Clock_Count + 1'b1;
+                if (r_Clock_Count < CLKS_PER_BIT-'d1) begin
+                    r_Clock_Count <= r_Clock_Count + 'd1;
                 end else begin
                     o_Rx_DV       <= 1'b1;
-                    r_Clock_Count <= '0;
+                    r_Clock_Count <= 0;
                     r_SM_Main     <= s_CLEANUP;
                 end
             end
 
             s_CLEANUP: begin
-                o_Rx_Byte <= r_Rx_Byte;
+                r_SM_Main <= s_IDLE;
                 o_Rx_DV   <= 1'b0;
-                r_SM_Main <= s_IDLE;
+                o_Rx_Byte <= r_Rx_Byte;
             end
 
-            default: begin
-                r_SM_Main <= s_IDLE;
-            end
-
+            default: r_SM_Main <= s_IDLE;
         endcase
     end
 end
